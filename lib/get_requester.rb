f0cefb6f2295ee00b8d7213d35d56926f0f1ed74
Mike Shahindoust <gobias101@gmail.com>
2020-09-03 00:10:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+
+
+class GetRequester
+  
+  attr_accessor :url
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body
+    
+  end
+  
+  def parse_json
+    
+  end
+  
+end
