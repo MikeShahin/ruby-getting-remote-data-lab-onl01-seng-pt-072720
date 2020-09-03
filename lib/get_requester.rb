ba9a78b4c13263bb10dbc5acf94110d2fc3a2ca8
Mike Shahindoust <gobias101@gmail.com>
2020-09-03 00:16:11 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,13 +10,13 @@ class GetRequester
     @url = url
   end
   
-  def get_response_body
-    uri - URI.parse(self.url)
+  def self.get_response_body
+    uri - URI.parse(url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
   
-  def parse_json
+  def self.parse_json
     data = JSON.parse(self.get_response_body)
     data
   end
